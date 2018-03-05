24382e16133ac567707505cbd8fd22fd5fdf7b0e
Uma <Uma@DESKTOP-CRHCDT9>
2018-03-05 06:19:10 +0530

    Complete Hybrid FrameWork

---
+++ FreeCRMTest/src/main/java/com/crm/qa/util/ExcelLib.java
@@ -0,0 +1,20 @@
+package com.crm.qa.util;
+
+import java.io.FileInputStream;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class ExcelLib {
+	public static String readExcel(String path, String sheet, int row, int cell){
+		String v="";
+		try {
+			FileInputStream fis=new FileInputStream(path);
+			Workbook wb=WorkbookFactory.create(fis);
+			v=wb.getSheet(sheet).getRow(row).getCell(cell).toString();
+		} 
+		catch (Exception e) {			
+		}
+		return v;
+	}
+}
---
+++ FreeCRMTest/src/main/java/com/crm/qa/util/ScreenShot.java
@@ -0,0 +1,28 @@
+package com.crm.qa.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class ScreenShot {
+
+	public static void getScreenshot(WebDriver driver, String folderpath) {
+		SimpleDateFormat s=new SimpleDateFormat("MM-dd-yyyy hh-mm-ss");
+		String dateTime=s.format(new Date());
+		
+		TakesScreenshot t=(TakesScreenshot) driver;
+		File srcFile = t.getScreenshotAs(OutputType.FILE);
+		File destFile=new File(folderpath+dateTime+".png");
+		
+		try {
+			FileUtils.copyFile(srcFile, destFile);
+		} catch (IOException e) {
+		}
+	}
+}
